044a2a07981d6ecbe13493fbcd1438bd4451c702
Jack CHEN Yan-Chia <jack.chen@titansoft.com.sg>
2023-07-18 21:26:14 +0800

    git init

---
+++ bin/dispatcher_pattern.dart
@@ -0,0 +1,5 @@
+import 'package:dispatcher_pattern/dispatcher_pattern.dart' as dispatcher_pattern;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dispatcher_pattern.calculate()}!');
+}
---
+++ test/dispatcher_pattern_test.dart
@@ -0,0 +1,8 @@
+import 'package:dispatcher_pattern/dispatcher_pattern.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

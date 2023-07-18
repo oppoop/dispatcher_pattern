65fbf7b35036ed7c97559adaa15e186e06576b1c
Jack CHEN Yan-Chia <jack.chen@titansoft.com.sg>
2023-07-19 00:06:11 +0800

    rename sample code

--- lib/dispatcher_pattern.dart
+++ lib/dispatcher_pattern.dart
@@ -2,14 +2,14 @@ import 'package:get_it/get_it.dart';
 
 GetIt getIt = GetIt.instance;
 
-class BlackCat {
+class Delivery {
   void delivery() {
     print('Black Cat Send');
   }
 }
 
 void main() {
-  getIt.registerSingleton<BlackCat>(BlackCat());
+  getIt.registerSingleton<Delivery>(Delivery());
 
-  getIt<BlackCat>().delivery();
+  getIt<Delivery>().delivery();
 }

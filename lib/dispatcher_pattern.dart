5150772839ccc5b62b999e37c07526d114ecf8a7
Jack CHEN Yan-Chia <jack.chen@titansoft.com.sg>
2023-07-19 00:35:58 +0800

    dispatcher sample code final

--- lib/dispatcher_pattern.dart
+++ lib/dispatcher_pattern.dart
@@ -2,14 +2,48 @@ import 'package:get_it/get_it.dart';
 
 GetIt getIt = GetIt.instance;
 
-class Delivery {
-  void delivery() {
+bool get getFeatureToggle => false;
+
+abstract class DeliveryHandler {
+  void handle() {}
+}
+
+class BlackCatHandler implements DeliveryHandler {
+  @override
+  void handle() {
     print('Black Cat Send');
+    // TODO: implement handle
+  }
+}
+
+class ChungHwaHandler implements DeliveryHandler {
+  @override
+  void handle() {
+    print('Chung Hwa Send');
+    // TODO: implement handle
+  }
+}
+
+class DeliveryDispatcher {
+  final bool isToggleOn;
+
+  final chungHwaHandler = ChungHwaHandler();
+
+  final blackCatHandler = BlackCatHandler();
+
+  DeliveryDispatcher(this.isToggleOn);
+
+  void delivery() {
+    if (isToggleOn) {
+      return chungHwaHandler.handle();
+    }
+    return blackCatHandler.handle();
   }
 }
 
 void main() {
-  getIt.registerSingleton<Delivery>(Delivery());
+  getIt.registerSingleton<DeliveryDispatcher>(
+      DeliveryDispatcher(getFeatureToggle));
 
-  getIt<Delivery>().delivery();
+  getIt<DeliveryDispatcher>().delivery();
 }

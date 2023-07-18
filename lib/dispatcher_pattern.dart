10bcad67867afd5eaa1552ef37c535e3cd44ec60
Jack CHEN Yan-Chia <jack.chen@titansoft.com.sg>
2023-07-19 00:04:35 +0800

    sample code init

--- lib/dispatcher_pattern.dart
+++ lib/dispatcher_pattern.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
+import 'package:get_it/get_it.dart';
+
+GetIt getIt = GetIt.instance;
+
+class BlackCatHandler {
+  void delivery() {
+    print('Black Cat Send');
+  }
+}
+
+void main() {
+  getIt.registerSingleton<BlackCatHandler>(BlackCatHandler());
+
+  getIt<BlackCatHandler>().delivery();
 }
